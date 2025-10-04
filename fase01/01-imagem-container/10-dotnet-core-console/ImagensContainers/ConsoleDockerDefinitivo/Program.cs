a77960cae94ceb8a5208f354f43f2e9550b6f4b9
pfjunior <pf.junior89@hotmail.com>
2025-10-04 15:39:54 -0300

    fase 1 - imagens e containers - aula 10

---
+++ fase01/01-imagem-container/10-dotnet-core-console/ImagensContainers/ConsoleDockerDefinitivo/Program.cs
@@ -0,0 +1,14 @@
+﻿string user = Environment.GetEnvironmentVariable("user");
+string password = Environment.GetEnvironmentVariable("password");
+
+string debug = Environment.GetEnvironmentVariable("debug");
+
+if (!string.IsNullOrWhiteSpace(debug))
+{
+    Console.WriteLine($"Connectionstring: {user}:{password} ");
+
+    Console.WriteLine(Environment.MachineName);
+    Console.WriteLine(Environment.OSVersion);
+}
+
+Console.WriteLine("Hello, World!");
