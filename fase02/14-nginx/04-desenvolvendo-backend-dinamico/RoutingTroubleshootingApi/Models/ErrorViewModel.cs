96912d333a956f34dcc3bac3a0e0cfa9e52ffe7f
pfjunior <pf.junior89@hotmail.com>
2025-11-21 15:57:30 -0300

    fase 2 - nginx - aula 04

---
+++ fase02/14-nginx/04-desenvolvendo-backend-dinamico/RoutingTroubleshootingApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoutingTroubleshootingApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
